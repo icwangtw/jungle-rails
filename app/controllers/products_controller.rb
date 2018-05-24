310c269c7907122977816c5812ec3ed00dd70502
Stephen <stephenicwang@gmail.com>
2018-05-24 01:53:17 +0000

    completed sold-out badge feature

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,19 @@
 class ProductsController < ApplicationController
+
+  helper_method :sold_out?
+
   def index
     @products = Product.all.order(created_at: :desc)
   end
 
   def show
     @product = Product.find params[:id]
+  end
+
+  def sold_out?(p)
+    if p.quantity == 0
+      true
+    end
   end
 
 end
