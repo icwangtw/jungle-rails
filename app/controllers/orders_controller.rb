43c631809544e10454ea433a1a4b82553bbee06a
Stephen <stephenicwang@gmail.com>
2018-05-25 01:21:30 +0000

    fixed receipt email feature

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,7 +11,7 @@ class OrdersController < ApplicationController
     if order.valid?
       empty_cart!
       redirect_to order, notice: 'Your Order has been placed.'
-      UserMailer.receipt_email(@order.id)
+      UserMailer.receipt_email(order)
     else
       redirect_to cart_path, flash: { error: order.errors.full_messages.first }
     end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,8 @@
 class UserMailer < ApplicationMailer
 
   default from: 'no-reply@jungle.com'
-  def receipt_email(orderid)
-    @order = Order.find_by(id: orderid)
-    mail(to: @order.email, subject: 'Thank for your order!')
+  def receipt_email(order)
+    @order = order
+    mail(to: @order.email, subject: 'Thank for your order! Your Order ID is #{orderid}')
   end
 end
